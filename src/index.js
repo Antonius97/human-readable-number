eb4a5df44ef88dc85a61eb8916271c9db36e3e41
Anton Merkulov <merkulov.anton.v@yandex.ru>
2020-07-14 19:03:49 +0300

    Solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+const nums = ["zero","one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+const decs = ["zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+module.exports = function toReadable(num = 0) {
+    if (!num) {
+        return nums[0];
+    }
+
+    let hund = Math.floor(num / 100);
+    let dec = Math.floor(num / 10 % 10);
+    let on = num % 10;
+
+    let out = [];
+
+    if (hund) {
+        out.push(nums[hund], "hundred");
+    }
+
+    if (dec > 1) {
+        out.push(decs[dec]);
+
+        if (on) {
+            out.push(nums[on]);
+        }
+    } else if (num % 100) {
+        out.push(nums[num % 100]);
+    }
+
+    return out.join(" ");
 }
